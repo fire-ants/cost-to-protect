1bb56b0b792474178612ac9d97113042e362d3fa
Aaron Sweers <aaron.sweers@gmail.com>
2019-04-19 14:53:43 -0500

    remove virtusteam

--- server.js
+++ server.js
@@ -63,63 +63,6 @@ app.get('/datadomain-ecs/graph/png', (req, res) => {
   });
 })
 
-
-app.get('/datadomain-virtustream', (req, res) => {
-  res.render('ddVirtustreamInput', {ddVirtustreamMenu: true, query: req.query, url: req._parsedOriginalUrl})
-})
-
-app.get('/datadomain-virtustream/graph', (req, res) => {
-  res.render('ddVirtustreamOutput', {ddVirtustreamMenu: true, query: req.query, url: req._parsedOriginalUrl, solution: getDataDomainVirtustreamSolution(req.query)})
-})
-
-app.get('/datadomain-virtustream/graph/png', (req, res) => {
-  const html = pug.renderFile('./views/ddVirtustreamExport.pug', {query: req.query, solution: getDataDomainVirtustreamSolution(req.query)});
-
-  const path = `./graph${new Date().getTime()}.png`;
-  webshot(html, path, {siteType:'html', renderDelay:1000}, (err) => {
-    console.log(`File created: ${path}`);
-    res.download(path, 'ddVirtustreamGraph.png', (err) => {
-      console.log(`File deleted: ${path}`)
-      fs.unlinkSync(path);
-    });
-  });
-})
-
-
-const getDataDomainVirtustreamSolution = function(query) {
-  const months = query.duration * 12;
-
-  const solution = {};
-  solution.size = calcGrowthArray(query.dataSet, query.growth, months);
-  solution.ddSize = calcGrowthArray(query.dataSet * (1 - (query.archive * 0.01)), query.growth, months);
-  solution.virtustreamSize = calcGrowthArray(query.dataSet * (query.archive * 0.01), query.growth, months);
-  solution.label = [];
-  solution.protectedCap = [];
-  solution.ddMoCost = [];
-  solution.cloudTierMoCost = [];
-  solution.virtustreamMoCost = [];
-  solution.virtustreamStorageType = getVirtustreamType(query.virtustreamCost);
-  solution.totalCost = [];
-
-  for (i = 0; i < months; i++) {
-    const totalGb = solution.size[i] * 1024;
-    const ddGb = solution.ddSize[i] * 1024;
-    const virtustreamGb = solution.virtustreamSize[i] * 1024;
-
-    solution.protectedCap.push(query.dedupRate * solution.size[i]);
-    solution.ddMoCost.push(query.ddCost / months / query.dedupRate / ddGb);
-    solution.cloudTierMoCost.push(query.cloudTierCost / months / query.dedupRate / virtustreamGb);
-    solution.virtustreamMoCost.push(query.virtustreamCost * 1);
-    solution.totalCost.push(solution.ddMoCost[i] + solution.cloudTierMoCost[i] + solution.virtustreamMoCost[i]);
-
-    solution.label.push(i+1);
-  }
-
-  solution.avgCost = calcAverage(solution.totalCost).toLocaleString('en-US', {minimumFractionDigits: 4});
-
-  return solution;
-}
-
 const getDataDomainEcsSolution = function(query) {
   const months = query.duration * 12;
 
@@ -190,20 +133,4 @@ const calcAverage = function(array) {
 
   var avg = sum/array.length;
   return avg;
-}
-
-const getVirtustreamType = function(cost) {
-  if (cost == 0.004167) {
-    return "Standard Infrequent Access"
-  }
-  else if (cost == 0.008333) {
-    return "Premium Infrequent Access"
-  }
-  else if(cost == 0.009831) {
-    return "Standard"
-  }
-  else if (cost == 0.019336) {
-    return "Premium"
-  }
-  return ""
 }
